e4ce39384bef7c6d219f0c44c0846f588c54037b
amgando <sherif@amgando.org>
2013-11-06 17:36:50 -0800

    feedback from Strand

--- app.js
+++ app.js
@@ -13,24 +13,30 @@ var Keystroker = function(mode){
 Keystroker.prototype.start_listening = function(trgt) {
   var target = trgt || document;
 
+  if(typeof target != "object") throw RangeError("target must be an object!");
+
   var strokes = this.strokes;
   var listeners = this.listeners;
 
   switch(this.mode){
     case 'keypress':
-      if (typeof target === "object" && 'onkeypress' in target) {
+      if ('onkeypress' in target) {
+        listeners[target + '.' + 'onkeypress'] = {target: target, type: 'onkeypress'};
+
         target.onkeypress = function(e){
           strokes.push({code: e.keyCode, char: String.fromCharCode(e.keyCode)})
-          listeners[e.target + '.' + e.type] = {target: e.target, type: e.type};
         };
+
       } else {
         throw new RangeError("target: [" + target + "] does not raise event: [" + this.mode + "]");
       }
       break;
+
     case 'keyup':
     case 'keydown':
       throw new SyntaxError("not implemented!");
       break;
+
     default:
       throw new RangeError("unsupported mode")
   }
@@ -47,10 +53,9 @@ Keystroker.prototype.show_listeners = function() {
 };
 
 Keystroker.prototype.nextKey = function(){
-  console.log(this.strokes);
-  var nk = this.strokes.pop();
-  if (null != nk) return nk['char']; // always return a single character OR nil
-  return null;
+  var key = this.strokes.pop();
+  if (null != key) return key['char']; // always return a single character
+  return null;                       // OR null
 }
 
 // Keystroker.prototype.verify = function(bool) {
@@ -61,6 +66,7 @@ Keystroker.prototype.nextKey = function(){
 // usage ...
 var k = new Keystroker();
 k.start_listening();
+k.show_listeners();
 ////////////////////////////////////////////////
 
 
