759b416499b57f4542a42a81b66428bf5ba9fd48
Bjgaynor <Bjgaynor@gmail.com>
2013-11-06 16:21:58 -0800

    additional refinements to the interface

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 ////////////////////////////////////////////////
-// Displayer (view-model)
-////////////////////////////////////////////////
-
-var Displayer = function(){}
-
-Displayer.prototype.nextChar = function(){}
-Displayer.prototype.verify = function(){}
-Displayer.prototype.done = function(){}
-
-
-////////////////////////////////////////////////
 // Keystroker (model)
 ////////////////////////////////////////////////
 
 var Keystroker = function(){}
 
-Keystroker.prototype.nextKey = function(){}
+Keystroker.prototype.nextKey = function(){
+  // replace the body of this function
+  return 'a'; // but always return a single character OR nil
+}
+
+
+////////////////////////////////////////////////
+// Displayer (view-model)
+////////////////////////////////////////////////
+
+var Displayer = function(){}
+
+Displayer.prototype.nextChar = function(){
+  // replace the body of this function
+  return 'a'; // but always return a single character
+}
+
+Displayer.prototype.verify = function(bool){
+  // replace the body of this function
+  return bool; // but keep this line
+}
+
+Displayer.prototype.done = function(){
+  // replace the body of this function
+  return false; // but always return a boolean
+}
 
 
 ////////////////////////////////////////////////
@@ -24,4 +38,13 @@ Keystroker.prototype.nextKey = function(){}
 
 var Comparer = function(){}
 
-Comparer.prototype.run = function(){}
+Comparer.prototype.run = function(){
+  // replace the body of this function
+  return true; // but always return a boolean
+}
+
+
+////////////////////////////////////////////////
+
+game = new Comparer();
+game.run();
