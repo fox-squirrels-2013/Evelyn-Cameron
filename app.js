5bef17783517e004db38903f0b4d39a3cf88dca7
amgando <sherif@amgando.org>
2013-11-06 17:27:33 -0800

    small fixes

--- app.js
+++ app.js
@@ -17,22 +17,18 @@ Keystroker.prototype.start_listening = function(trgt) {
   var listeners = this.listeners;
 
   switch(this.mode){
-    case 'keydown':
-      throw new SyntaxError("not implemented!");
-      break;
     case 'keypress':
-      console.log(target);
-      if ('onkeypress' in target) {
+      if (typeof target === "object" && 'onkeypress' in target) {
         target.onkeypress = function(e){
           strokes.push({code: e.keyCode, char: String.fromCharCode(e.keyCode)})
           listeners[e.target + '.' + e.type] = {target: e.target, type: e.type};
         };
+      } else {
+        throw new RangeError("target: [" + target + "] does not raise event: [" + this.mode + "]");
       }
-      //  else {
-      //   throw new RangeError("target: [" + target + "] does not raise event: [" + this.mode + "]");
-      // }
       break;
     case 'keyup':
+    case 'keydown':
       throw new SyntaxError("not implemented!");
       break;
     default:
@@ -51,6 +47,7 @@ Keystroker.prototype.show_listeners = function() {
 };
 
 Keystroker.prototype.nextKey = function(){
+  console.log(this.strokes);
   var nk = this.strokes.pop();
   if (null != nk) return nk['char']; // always return a single character OR nil
   return null;
@@ -64,7 +61,6 @@ Keystroker.prototype.nextKey = function(){
 // usage ...
 var k = new Keystroker();
 k.start_listening();
-k.show_listeners();
 ////////////////////////////////////////////////
 
 
