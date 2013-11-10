f180c83aa4b4e40f89518176f603ba252e471d7e
Sherif Abushadi <github@amgando.org>
2013-11-09 20:05:03 -0800

    added simple anomation to screen

--- app.js
+++ app.js
@@ -65,28 +65,59 @@ Keystroker.prototype.nextKey = function(){
 
 var Displayer = function(){
   this.index = 0;
-  this.displayText = document.getElementById('displayText').innerHTML;
+  this.content = $('#content').text();
+};
+
+Displayer.prototype.highlight = function() {
+    var pre = '<span style="font-weight:bold;color:red;">'
+    var post = '</span>'
+    var symbol = '<span style="color:red;">*</span>';
+
+    if (this.nextChar() === ' ') {
+      this.insert(this.index, symbol)
+    } else {
+      this.surround(this.index, pre, post );
+    }
+
 };
 
 Displayer.prototype.getGametext = function() {
-  return this.displayText;
+  return this.content;
 };
 
 Displayer.prototype.verify = function(bool) {
-  if(bool){ this.index++ }
+  if(bool) { 
+    this.index++;
+    this.highlight();
+  }
   return this.nextChar();
 };
 
 Displayer.prototype.done = function() {
-  if(this.index >= this.displayText.length){
-    $("div").animate({ height:'toggle'},'slow');
+  if(this.index >= this.content.length){
+    $('#content').animate({ height:'toggle'},'slow');
     return true;
   }
   return false;
 };
 
 Displayer.prototype.nextChar = function() {
-  return this.displayText.charAt(this.index);
+  return this.content.charAt(this.index);
+};
+
+// use this to insert something like an asterisk or whatever to follow the typist
+Displayer.prototype.insert = function(position, symbol) {
+  var updated_content = this.content.substr(0, position) + symbol +
+                        this.content.substr(position);
+  $('#content').html(updated_content);
+};
+
+// use this to surround the current letter as a hint for the typist
+Displayer.prototype.surround = function(position, before, after) {
+  var updated_content = this.content.substr(0, position) + before +
+                        this.nextChar() + after + 
+                        this.content.substr(position +1);
+  $('#content').html(updated_content);
 };
 
 ////////////////////////////////////////////////
@@ -106,11 +137,13 @@ Displayer.prototype.nextChar = function() {
 // Comparer (controller)
 ////////////////////////////////////////////////
 
-var KEY_SAMPLE_INTERVAL = 50 ;
-
+var KEY_SAMPLE_INTERVAL = 200 ;
+var PATIENCE = 20 // the number of cycle we wait for you to type something til we stop the game
 var Comparer = function(){
   this.user = new Keystroker();
   this.disp = new Displayer();
+  this.errors = [];
+  this.silence = 0;
 }
 
 Comparer.prototype.run = function(){
@@ -123,15 +156,27 @@ Comparer.prototype.run = function(){
   var handle = window.setInterval(function(){
     console.log(self.user.allKeys());
 
-    if(self.disp.done()) { clearInterval(handle); }
+    if(self.disp.done() || self.silence > PATIENCE) { 
+      clearInterval(handle);
+      console.log('game over!');
+      return;
+    }
 
-    if(!self.keyMatchedChar(k, d)){
-      console.log('keep trying...['+k+'] doesn\'t match ['+d+']');
+    if(null === k) {
+      console.log('type something!');
       k = self.user.nextKey();
+      self.silence++;
+
+    } else if(!self.keyMatchedChar(k, d)){
+      console.log('keep trying...['+k+'] doesn\'t match ['+d+']' + ' error count: '+ self.errors.length);
+      self.errors.push(k);
+      k = self.user.nextKey();
+      self.silence = 0;
     } else {
       console.log('match! nicely done. ['+k+','+d+']');
       k = self.user.nextKey();
       d = self.disp.verify(true);
+      self.silence = 0;
     }
   }, KEY_SAMPLE_INTERVAL);
 }
@@ -143,5 +188,8 @@ Comparer.prototype.keyMatchedChar = function(userChar, gameChar){
 
 ////////////////////////////////////////////////
 
-game = new Comparer();
-game.run();
+$('document').ready(function(){
+  game = new Comparer();
+  game.run();
+
+});
